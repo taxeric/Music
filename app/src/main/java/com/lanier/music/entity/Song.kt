16ce4b4281e3ede52aeba39b2621e33010b182a0
幻弦让叶 <1248389474@qq.com>
2023-04-07 20:49:35 +0800

    新增uri属性

--- app/src/main/java/com/lanier/music/entity/Song.kt
+++ app/src/main/java/com/lanier/music/entity/Song.kt
@@ -1,5 +1,7 @@
 package com.lanier.music.entity
 
+import android.net.Uri
+
 /**
  * Create by Eric
  * on 2023/4/1
@@ -8,6 +10,7 @@ data class Song (
     val id: Long,
     val path: String,
     val name: String,
+    val pathUri: Uri? = null,
     val duration: Long = 0L
 ) {
 
